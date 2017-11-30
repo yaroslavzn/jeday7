c1a6ad8310f87859e1afa2bf74828836cc5cd3ca
Yaroslav Mishcneko <yaroslavzn@gmail.com>
2017-11-30 14:55:39 +0200

    change gitignore file

---
+++ build/scripts/main.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    $('#my-menu').mmenu({
+        extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black'],
+        navbar: {
+            title: '<img src="images/logo-1.svg" alt="Салон красоты S&Mitler"/>'
+        },
+        offCanvas: {
+            position: 'right'
+        }
+    });
+
+    var api = $('#my-menu').data('mmenu');
+
+    api.bind('opened', function () {
+        $('.hamburger').addClass('is-active');
+    });
+
+    api.bind('closed', function () {
+        $('.hamburger').removeClass('is-active');
+    });
+});
