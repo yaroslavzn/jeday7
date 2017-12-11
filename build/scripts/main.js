2882375538405b74365cf0a5f2bef6d067e43d12
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2017-12-11 17:52:07 +0200

    add partners section

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -77,6 +77,29 @@ $(function () {
         smartSpeed: 700,
         nav: false
     });
+
+    $('.partners').owlCarousel({
+        loop: true,
+        smartSpeed: 700,
+        dots: false,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
     // Resize window
 
 
