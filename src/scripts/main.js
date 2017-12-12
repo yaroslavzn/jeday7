3221355cd98d22965b2306f986b9e7a39a3b23be
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2017-12-12 15:20:50 +0200

    finish project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -129,4 +129,26 @@ $(function () {
         });
         return false;
     });
+    
+    // back to top button
+    
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > $(this).height()) {
+           $('.top').addClass('active');
+       } else {
+           $('.top').removeClass('active');
+       }
+    });
+
+    $('.top').click(function () {
+        $('html, body').stop().animate({
+            scrollTop: 0
+        }, 'slow', 'swing');
+    });
+
+    // preloader function
+
+    $(window).on('load', function () {
+        $('.preloader').delay(750).fadeOut('slow');
+    });
 });
